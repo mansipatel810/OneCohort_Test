ee2dce9f4fc9d485df5cf72b9aaadf6f78c5fb25
Gnaneshwar <gnaneshwarreddyyenna@gmail.com>
2026-05-19 10:14:20 +0530

    made a change to testListener:

--- src/test/java/com/cts/mfrp/onecohort/listeners/TestListener.java
+++ src/test/java/com/cts/mfrp/onecohort/listeners/TestListener.java
@@ -1,4 +1,15 @@
 package com.cts.mfrp.onecohort.listeners;
 
-public class TestListener {
+import org.testng.ITestListener; // 1. Add this import
+import org.testng.ITestResult;
+
+// 2. Add "implements ITestListener" to your class declaration
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        // Your logic here
+    }
+
+    // You can override other methods like onTestSuccess, onTestFailure, etc.
 }
